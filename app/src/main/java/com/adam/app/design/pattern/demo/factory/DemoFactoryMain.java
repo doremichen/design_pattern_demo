b7ed1b46c9bb9a49abdb5c3626a4c9af6dda6745
AdamChen <powq2011@gmail.com>
2025-07-15 08:47:55 +0800

    refacor demo factory pattern

--- app/src/main/java/com/adam/app/design/pattern/demo/factory/DemoFactoryMain.java
+++ app/src/main/java/com/adam/app/design/pattern/demo/factory/DemoFactoryMain.java
@@ -2,37 +2,23 @@
  * Description: This is the main activity of the factory pattern demo.
  *            It shows the instruction and a button to start the demo.
  * Author: Adam Chen
- * Date: 2025/07/03
+ * Date: 2025/07/15
  */
 package com.adam.app.design.pattern.demo.factory;
 
-import android.content.Intent;
-import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import com.adam.app.design.pattern.demo.BaseInstructionActivity;
 import com.adam.app.design.pattern.demo.R;
-import com.adam.app.design.pattern.demo.databinding.ActivityDemoFactoryMainBinding;
+import com.adam.app.design.pattern.demo.Util;
 
-public class DemoFactoryMain extends AppCompatActivity {
-
-    // view binding
-    private ActivityDemoFactoryMainBinding mBinding;
+public class DemoFactoryMain extends BaseInstructionActivity {
+    @Override
+    public String getInstruction() {
+        return getString(R.string.demo_factory_instruction);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // view binding
-        mBinding = ActivityDemoFactoryMainBinding.inflate(getLayoutInflater());
-        setContentView(mBinding.getRoot());
-        // set title
-        setTitle(R.string.title_activity_demo_factory_main);
-
-        // set start button listener
-        mBinding.btnStartFactoryDemo.setOnClickListener(v -> {
-            Intent intent = new Intent(this, DemoFactoryStart.class);
-            startActivity(intent);
-        });
-
+    public void startDemo() {
+        // start demo factory activity
+        Util.startActivity(this, DemoFactoryStart.class);
     }
 }
